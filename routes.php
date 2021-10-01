93bc36ab3740bd5344cc2279911d5aebca6cd169
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-01 13:25:47 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,1 +1,23 @@
+<?php
+$controllers = array('pages'=>['home','error']); //list controller and action
+function call($controller,$action)
+{
+    echo "routes to ".$comtroller."-".$action."<br>";
+}
 
+if(array_key_exists($controller,$controllers))
+{
+    if(in_array($action,$controllers[$controller]))
+    {
+      call($controller,$action);
+    }
+    else
+    {
+      call('pages','error');
+    }
+}
+else
+{
+  call('pages','error');
+}
+?>
