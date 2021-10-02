8509f0150009b4b443567ddf0b7ae6ec280ad59d
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-02 15:40:24 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -3,11 +3,11 @@ class PagesController
 {
   public function home()
   {
-    require_once('views/Pages/home.php');
+    require_once('./views/Pages/home.php');
   }
   public function error()
   {
-    require_once('views/Pages/error.php');
+    require_once('./views/Pages/error.php');
   }
 }
 ?>
