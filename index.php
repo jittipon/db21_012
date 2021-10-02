3f414e241623019dabe253c9fad4eb9192be6873
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-02 15:22:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,11 @@ else
   $action = 'home';
 }
 ?>
-
 <html>
 <head></head>
 <body>
    <?php echo "controller = ".$controller.",action = ".$action.;?>
-   <br>[<a href="">Home </a>] <br>
+   //<br>[<a href="">Home </a>] <br>
    <?php require_once("routes.php");?>
 </body>
 </html> 
