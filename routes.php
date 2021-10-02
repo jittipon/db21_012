971363c37712aba1d71d371b6f509265c553348f
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-02 15:17:16 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,13 @@
 $controllers = array('pages'=>['home','error']); //list controller and action
 function call($controller,$action)
 {
-    echo "routes to ".$controller."-".$action."<br>";
+    require_once();
+    switch($controller)
+    {
+        case "pages":   $controller = new PagesController();
+                        break;
+    }
+    $controller->{$action}();
 }
 
 if(array_key_exists($controller,$controllers))
