bf94a3a2220cea61781051d7470793edb7134abb
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-03 12:51:35 +0700

    Update orderModel.php

--- models/orderModel.php
+++ models/orderModel.php
@@ -1,2 +1,26 @@
 <?php
+class Order
+  public $orderId,$orderDate;
+
+punlic function_construct($orderId,$orderDate)
+{
+    $this->orderId = $orderId;
+    $this->orderDate = $orderDate;
+}
+
+public static function getAll()
+{
+    $orderList = [];
+    require("connection_connect.php");
+    $sql = "select * from OrderOut";
+    $result = $conn->query($sql);
+    while($my_roow == $result->fetch_assoc())
+    {
+        $order_id = $my_row[OrderOut_ID];
+        $order_date = $my_row[OrderOut_DateOrder];
+        $orderList[] = new Order($order_id,$order_date);
+    }
+    require("connection_close.php");
+    return $orderList;
+}
 ?>
