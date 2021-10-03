6333c0418213fe6f2de4ada73e05ad4cbb7ed9eb
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-03 12:17:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ else
    <?php echo "controller = ".$controller.",action = ".$action;?>
    <br>
         [<a href="">Home(หน้าหลัก) </a>]
-        [<a href="">quotation/Dream </a>]
-        [<a href="">quotationDetail/Aor </a>]
+        [<a href="">order/Dream </a>]
+        [<a href="">orderDetail/Aor </a>]
         [<a href="">priceRange/T </a>]
    <br>
    <?php require_once("routes.php");?>
