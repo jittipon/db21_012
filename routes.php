e6c4d894e602edffeafe64fb2e505af1c459aede
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-03 15:12:44 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,11 +1,16 @@
 <?php
 $controllers = array('pages'=>['home','error']); //list controller and action
+$controllers = array('pages'=>['home','error'],'order'=>['index']);
+
 function call($controller,$action)
 {
     require_once("controllers/".$controller."_controller.php");
     switch($controller)
     {
         case "pages":   $controller = new PagesController();
+                        break;
+        case "order":   require_once("models/orderModel.php");
+                        $controller = new OrderController();
                         break;
     }
     $controller->{$action}();
