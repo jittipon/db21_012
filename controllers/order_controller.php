1a721c5ae9d2229f56c50e8125db51ad41471dca
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 12:01:55 +0700

    Update order_controller.php

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -4,6 +4,7 @@ class OrderController
 {
     public function index()
     {
+        echo "plai";
         $order_list = Order::getAll();
         require_once('views/order/index_order.php');
     }
