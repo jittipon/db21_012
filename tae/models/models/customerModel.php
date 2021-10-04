4a3249c80345bfce37c46d9e645ec20527f80295
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 23:34:33 +0700

    Revert "Merge branch 'main' of https://github.com/jittipon/db21_012 into main"
    This reverts commit db78ff255b6340930a523a670774b45241cf8211, reversing
    changes made to 2b087ca25b30f9e19d5e4af158978cef21f4b2b5.

---
+++ tae/models/models/customerModel.php
@@ -0,0 +1,27 @@
+<?php
+class Customer
+{
+    public $id, $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+    public static function getAll()
+    {
+        $customerList = [];
+        require("connection_connect.php");
+        $sql = "select * from Customer";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()) 
+        {
+            $CusID = $my_row[Customer_ID];
+            $CusName = $my_row[Customer_Name];
+            $customerList[] = new Customer($CusID, $CusName);
+        }
+        require("connection_close.php");
+        return $customerList;
+    }
+}
+?>
---
+++ tae/models/models/orderModel.php
@@ -0,0 +1,108 @@
+<?php
+class Order
+{
+    public $orderId;
+    public $orderDate;
+    public $customerId;
+    public $customerName;
+    public $employeeId;
+    public $employeeName;
+
+  public function __construct($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName)
+  {
+      $this->orderId = $orderId;
+      $this->orderDate = $orderDate;
+      $this->customerId = $customerId;
+      $this->customerName = $customerName;
+      $this->employeeId = $employeeId;
+      $this->employeeName = $employeeName;
+  }
+
+  public static function get($orderId)
+  {
+      require("connection_connect.php");
+      $sql = "SELECT OrderOut_ID,OrderOut_DateOrder,Customer_Name,Employee_Name FROM OrderOut
+                NATURAL JOIN Customer NATURAL JOIN Employee
+                WHERE OrderOut_ID = '$orderId'";
+      $result = $conn->query($sql);
+      $my_row = $result->fetch_assoc();
+      $orderId = $my_row["OrderOut_ID"];
+      $orderDate = $my_row["OrderOut_DateOrder"];
+      $customerId = $my_row["Customer_ID"];
+      $customerName = $my_row["Customer_Name"];
+      $employeeId = $my_row["Employee_ID"];
+      $employeeName = $my_row["Employee_Name"];
+      require("connection_close.php");
+      return new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+  }
+    
+  public static function getAll()
+  {
+      $orderList = [];
+      require("connection_connect.php");
+      $sql = "SELECT OrderOut_ID,OrderOut_DateOrder,Customer_Name,Employee_Name FROM OrderOut
+                NATURAL JOIN Customer NATURAL JOIN Employee";
+      $result = $conn->query($sql);
+      while($my_row = $result->fetch_assoc())
+      {
+            $orderId = $my_row["OrderOut_ID"];
+            $orderDate = $my_row["OrderOut_DateOrder"];
+            $customerId = $my_row["Customer_ID"];
+            $customerName = $my_row["Customer_Name"];
+            $employeeId = $my_row["Employee_ID"];
+            $employeeName = $my_row["Employee_Name"];
+            $orderList[] = new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+      }
+      require("connection_close.php");
+      return $orderList;
+  }
+  
+  public static function search($key)
+  {
+      $orderList = [];
+      require("connection_connect.php");
+      $sql = "";
+      $result = $conn->query($sql);
+      while($my_roow = $result->fetch_assoc())
+      {
+            $orderId = $my_row["OrderOut_ID"];
+            $orderDate = $my_row["OrderOut_DateOrder"];
+            $customerId = $my_row["Customer_ID"];
+            $customerName = $my_row["Customer_Name"];
+            $employeeId = $my_row["Employee_ID"];
+            $employeeName = $my_row["Employee_Name"];
+            $orderList[] = new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+      }
+      require("connection_close.php");
+      return $orderList;
+  }
+    
+  public static function add($orderId,$orderDate,$customerId,$employeeId)
+  {
+      require("connection_connect.php");
+      $sql = "INSERT INTO 'OrderOut' ('OrderOut_ID','OrderOut_DateOrder','Customer_ID','Employee_ID') VALUES
+             ('$orderId','$orderDate','$customerId','$employeeId')";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Add Success $result rows";
+  }
+    
+  public static function update($orderId,$orderDate,$customerId,$employeeId)
+  {
+      require("connection_connect.php");
+      $sql = "UPDATE OrderOut SET OrderOut_DateOrder = '$orderDate',Employee_ID = '$employeeId',Customer_ID = 'customerId' WHERE OrderOut.Order_ID = '$orderId'";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Update Success $result rows";
+  }
+  
+  public static function delete($orderId)
+  {
+      require("connection_connect.php");
+      $sql = "DELETE FROM OrderOut WHERE OrderOut.Order_ID = '$orderId'";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Delete Success $result rows";
+  }
+}
+?>
