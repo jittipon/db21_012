dc3d34915dd45e037e372b7dcbaf6dc174682a74
Phalaenopsis <chanwit.ja@ku.th>
2021-10-04 23:07:58 +0700

    ทำ add orderDetail

--- connection_connect.php
+++ connection_connect.php
@@ -4,14 +4,13 @@ $username = "db21_012";
 $password = "db21_012";
 $dbname = "db21_012";
 $conn = new mysqli($servername,$username,$password);
-
-$conn->set_charset("utf8");
+mysqli_set_charset($conn, "utf8");
 if($conn->connect_error)
 {
-  die("Connection failed: ".$conn->connect_error);
+  die("Connection failed: ". $conn->connect_error);
 }
 if(!$conn->select_db($dbname))
 {
-  die("Connection failed: ".$conn->connect_error);
+  die("Connection failed: ". $conn->connect_error);
 }
 ?>
