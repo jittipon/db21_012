b53f7e98ca16d79767d1f50e18a478f33f151004
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 20:14:52 +0700

    1

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -7,5 +7,13 @@ class OrderController
         $order_list = Order::getAll();
         require_once('views/order/index_order.php');
     }
+
+    public function newOrder()
+    {
+        $order_list = Order::getAll();
+        $employee_list = Employee::getAll();
+        $customer_list = Customer::getAll();
+        require_once('views/order/newOrder.php');
+    }
 }
 ?>
