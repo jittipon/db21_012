a308d729cbc6efafd7b823fb254790ca4b384e37
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 10:42:04 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,8 +1,25 @@
 <table border = 1>
-<tr> <td>ID
-
-  <?php
-  foreach($order_list as $order)
+<tr>
+     <td>OrderID</td>
+     <td>OrderDate</td>
+     <td>CustomerName</td>
+     <td>EmployeeName</td>
+     <td>update</td>
+     <td>delete</td>
+</tr>
+  
+<?php
+  foreach($order_list as $OrderOut)
   {
-      echo "
+      echo 
+      "<tr>
+      <td>$OrderOut->orderId</td>
+      <td>$OrderOut->orderDate</td>
+      <td>$OrderOut->customerName</td>
+      <td>$OrderOut->employeeName</td>
+      <td>$OrderOut->update</td>
+      <td>$OrderOut->delete</td>
+      </tr>";
   }
+echo "</table>";
+?>
