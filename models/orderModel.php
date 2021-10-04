be2fa6304fcaeb8ac8784f7ebbe23a6ac30e7dfb
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 08:59:51 +0700

    Update orderModel.php

--- models/orderModel.php
+++ models/orderModel.php
@@ -1,27 +1,28 @@
 <?php
 class Order
-  public $orderId;
-  public $orderDate;
-  public $customerId;
-  public $customerName;
-  public $employeeId;
-  public $employeeName;
-
-public function _construct($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName)
 {
-    $this->orderId = $orderId;
-    $this->orderDate = $orderDate;
-    $this->customerId = $customerId;
-    $this->customerName = $customerName;
-    $this->employeeId = $employeeId;
-    $this->employeeName = $employeeName;
-}
+    public $orderId;
+    public $orderDate;
+    public $customerId;
+    public $customerName;
+    public $employeeId;
+    public $employeeName;
 
-public static function get($orderId)
-{
-    require("connection_connect.php");
-    $sql = "";
-}
+  public function _construct($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName)
+  {
+      $this->orderId = $orderId;
+      $this->orderDate = $orderDate;
+      $this->customerId = $customerId;
+      $this->customerName = $customerName;
+      $this->employeeId = $employeeId;
+      $this->employeeName = $employeeName;
+  }
+
+  public static function get($orderId)
+  {
+      require("connection_connect.php");
+      $sql = "";
+  }
 
 
 
@@ -33,19 +34,20 @@ public static function get($orderId)
 
 
 
-public static function getAll()
-{
-    $orderList = [];
-    require("connection_connect.php");
-    $sql = "select * from OrderOut";
-    $result = $conn->query($sql);
-    while($my_roow == $result->fetch_assoc())
-    {
-        $order_id = $my_row[OrderOut_ID];
-        $order_date = $my_row[OrderOut_DateOrder];
-        $orderList[] = new Order($order_id,$order_date);
-    }
-    require("connection_close.php");
-    return $orderList;
+  public static function getAll()
+  {
+      $orderList = [];
+      require("connection_connect.php");
+      $sql = "select * from OrderOut";
+      $result = $conn->query($sql);
+      while($my_roow == $result->fetch_assoc())
+      {
+          $order_id = $my_row[OrderOut_ID];
+          $order_date = $my_row[OrderOut_DateOrder];
+          $orderList[] = new Order($order_id,$order_date);
+      }
+      require("connection_close.php");
+      return $orderList;
+  }
 }
 ?>
