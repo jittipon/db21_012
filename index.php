dd62a23583fb1d2fa5375af09476c7e1af965257
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 23:34:30 +0700

    Revert "Merge branch 'main' of https://github.com/jittipon/db21_012 into main"
    This reverts commit 2b087ca25b30f9e19d5e4af158978cef21f4b2b5, reversing
    changes made to 2f603dba74d5da0d8b7cc3e71408655bc51bf589.

--- index.php
+++ index.php
@@ -13,16 +13,13 @@ else
 <html>
 <head></head>
 <body>
-   <?php //echo "controller = ".$controller.",action = ".$action;?>
-  
-    
+   <?php echo "controller = ".$controller." , action = ".$action;?>
    <br>
-        [<a href="">Home(หน้าหลัก) </a>]
-        [<a href="?controller=order&action=index">order/Dream </a>]
+        [<a href="?controller=pages&action=home">Home(หน้าหลัก) </a>]
+        [<a href="?controller=order&action=index">order </a>]
         [<a href="?controller=orderDetail&action=index">orderDetail </a>]
-        [<a href="?controller=priceRange&action=index">priceRange/T </a>]
+        [<a href="?controller=priceRange&action=index">priceRange </a>]
    <br>
-   <?php echo "controller = ".$controller.",action = ".$action;?>
    <?php require_once("routes.php");?>
 </body>
 </html> 
--- models/orderModel.php
+++ models/orderModel.php
@@ -1,26 +1,111 @@
 <?php
 class Order
-  public $orderId,$orderDate;
-
-punlic function_construct($orderId,$orderDate)
 {
-    $this->orderId = $orderId;
-    $this->orderDate = $orderDate;
-}
+    public $orderId;
+    public $orderDate;
+    public $customerId;
+    public $customerName;
+    public $employeeId;
+    public $employeeName;
 
-public static function getAll()
-{
-    $orderList = [];
-    require("connection_connect.php");
-    $sql = "select * from OrderOut";
-    $result = $conn->query($sql);
-    while($my_roow == $result->fetch_assoc())
-    {
-        $order_id = $my_row[OrderOut_ID];
-        $order_date = $my_row[OrderOut_DateOrder];
-        $orderList[] = new Order($order_id,$order_date);
-    }
-    require("connection_close.php");
-    return $orderList;
+  public function __construct($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName)
+  {
+      $this->orderId = $orderId;
+      $this->orderDate = $orderDate;
+      $this->customerId = $customerId;
+      $this->customerName = $customerName;
+      $this->employeeId = $employeeId;
+      $this->employeeName = $employeeName;
+  }
+
+  public static function get($orderId)
+  {
+      require("connection_connect.php");
+      $sql = "SELECT OrderOut_ID,OrderOut_DateOrder,Customer_Name,Employee_Name FROM OrderOut
+                NATURAL JOIN Customer NATURAL JOIN Employee
+                WHERE OrderOut_ID = '$orderId'";
+      $result = $conn->query($sql);
+      $my_row = $result->fetch_assoc();
+      $orderId = $my_row["OrderOut_ID"];
+      $orderDate = $my_row["OrderOut_DateOrder"];
+      $customerId = $my_row["Customer_ID"];
+      $customerName = $my_row["Customer_Name"];
+      $employeeId = $my_row["Employee_ID"];
+      $employeeName = $my_row["Employee_Name"];
+      require("connection_close.php");
+      return new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+  }
+    
+  public static function getAll()
+  {
+      $orderList = [];
+      require("connection_connect.php");
+      $sql = "SELECT OrderOut_ID,OrderOut_DateOrder,Customer_Name,Employee_Name FROM OrderOut
+                NATURAL JOIN Customer NATURAL JOIN Employee
+                ORDER BY OrderOut_ID";
+      $result = $conn->query($sql);
+      while($my_row = $result->fetch_assoc())
+      {
+            $orderId = $my_row["OrderOut_ID"];
+            $orderDate = $my_row["OrderOut_DateOrder"];
+            $customerId = $my_row["Customer_ID"];
+            $customerName = $my_row["Customer_Name"];
+            $employeeId = $my_row["Employee_ID"];
+            $employeeName = $my_row["Employee_Name"];
+            $orderList[] = new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+      }
+      require("connection_close.php");
+      return $orderList;
+  }
+  
+  public static function search($key)
+  {
+      $orderList = [];
+      require("connection_connect.php");
+      $sql = "SELECT OrderOut_ID,OrderOut_DateOrder,Customer_Name,Employee_Name FROM OrderOut
+                NATURAL JOIN Customer NATURAL JOIN Employee
+                WHERE (OrderOut_ID LIKE '%$key%' OR OrderOut_DateOrder LIKE '%$key%' OR Customer_Name LIKE '%$key%' OR Employee_Name LIKE '%$key%')";
+      $result = $conn->query($sql);
+      while($my_row = $result->fetch_assoc())
+      {
+            $orderId = $my_row["OrderOut_ID"];
+            $orderDate = $my_row["OrderOut_DateOrder"];
+            $customerId = $my_row["Customer_ID"];
+            $customerName = $my_row["Customer_Name"];
+            $employeeId = $my_row["Employee_ID"];
+            $employeeName = $my_row["Employee_Name"];
+            $orderList[] = new Order($orderId,$orderDate,$customerId,$customerName,$employeeId,$employeeName);
+      }
+      require("connection_close.php");
+      return $orderList;
+  }
+    
+  public static function add($orderId,$orderDate,$customerId,$employeeId)
+  {
+      require("connection_connect.php");
+      echo "$orderId,$orderDate,$customerId,$employeeId";
+      $sql = "INSERT INTO `OrderOut` (`OrderOut_ID`, `OrderOut_DateOrder`, `Customer_ID`, `Employee_ID`, `OrderOut_PaymentCondition`, `OrderOut_EarnestMoneyREQ`, `OrderOut_EarnestMoneyREQStatus`, `OrderOut_EarnestMoneyPaidDate`, `Approver_ID`, `OrderOut_DateApprove`, `OrderOut_ReservePercent`, `OrderOut_Vat`, `OrderOut_Status`, `OrderOut_DeliveryStatus`) VALUES ('$orderId', '$orderDate', '$customerId', '$employeeId', '0', NULL, NULL, NULL, NULL, NULL, NULL, '0.7', 'offered', NULL);";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Add Success rows";
+  }
+    
+  public static function update($orderId,$orderDate,$customerId,$employeeId)
+  {
+      require("connection_connect.php");
+      $sql = "UPDATE OrderOut SET OrderOut_DateOrder = '$orderDate',Employee_ID = '$employeeId',Customer_ID = '$customerId' WHERE OrderOut.Order_ID = '$orderId'";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Update Success rows";
+  }
+  
+  public static function delete($orderId)
+  {
+      require("connection_connect.php");
+      $sql = "DELETE FROM OrderOut WHERE OrderOut.Order_ID = '$orderId'";
+      $result = $conn->query($sql);
+      require("connection_close.php");
+      return "Delete Success rows";
+  }
 }
 ?>
