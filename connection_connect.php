9728ad6a84605f501948c28e2db74f1a60895fc5
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-04 11:10:00 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -6,10 +6,10 @@ $dbname = "db21_012";
 $conn = new mysqli($servername,$username,$password);
 if($conn->connect_error)
 {
-  die("Connection failed: ".$conn->connect_error);
+  die("Connection failed: ". $conn->connect_error);
 }
 if(!$conn->select_db($dbname))
 {
-  die("Connection failed: ".$conn->connect_error);
+  die("Connection failed: ". $conn->connect_error);
 }
 ?>
