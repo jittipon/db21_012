2d4565b6dbb4ddcceec91cdfb91cc07e842bcbf3
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2021-10-05 10:46:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,9 @@ else
     
    <br>
         [<a href="">Home(หน้าหลัก) </a>]
-        [<a href="?controller=order&action=index">order/Dream </a>]
+        [<a href="?controller=order&action=index">order </a>]
         [<a href="?controller=orderDetail&action=index">orderDetail </a>]
-        [<a href="?controller=priceRange&action=index">priceRange/T </a>]
+        [<a href="?controller=priceRange&action=index">priceRange </a>]
    <br>
    <?php echo "controller = ".$controller.",action = ".$action;?>
    <?php require_once("routes.php");?>
